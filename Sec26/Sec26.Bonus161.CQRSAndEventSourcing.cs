510ecdf16be3d2912125aa60ac8cba9712107ad5
Charlie Zhipeng Li <zhipli@microsoft.com>
2021-06-25 15:37:05 -0700

    Added new file

--- Sec26/Sec26.Bonus161.CQRSAndEventSourcing.cs
+++ Sec26/Sec26.Bonus161.CQRSAndEventSourcing.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac.Core.Activators;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Execution;
 using System;
 using System.CodeDom;
 using System.Collections;
@@ -18,6 +19,115 @@ using static System.Console;
 
 namespace Sec26.Bonus161
 {
+    // CQRS = Command query responsibility segregation
+    // CQS = Command query separation
+
+    // Command = do/change
+    public class Person
+    {
+        private int age;
+        EventBroker broker;
+        public Person(EventBroker broker)
+        {
+            this.broker = broker;
+            broker.Commands += BrokerOnCommands;
+            broker.Queries += BrokerOnQueries;
+        }
+
+        private void BrokerOnQueries(object sender, Query query)
+        {
+            var ac = query as AgeQuery;
+            if (ac != null && ac.Target == this)
+            {
+                ac.Result = age;
+            }
+        }
+
+        private void BrokerOnCommands(object sender, Command e)
+        {
+            var cac = e as ChangeAgeCommand;
+            if (cac != null && cac.Target == this)
+            {
+                if(cac.Register)
+                    broker.AllEvents.Add(new AgeChangedEvent(this, age, cac.Age));
+                this.age = cac.Age;
+            }
+        }
+    }
+    public class EventBroker
+    {
+        // 1. All events that happened
+        public IList<Event> AllEvents = new List<Event>();
+        // 2. Commands
+        public event EventHandler<Command> Commands;
+        // 3. Query
+        public event EventHandler<Query> Queries;
+
+        public void Command(Command c)
+        {
+            Commands?.Invoke(this, c);
+        }
+        public T Query<T>(Query q)
+        {
+            Queries?.Invoke(this, q);
+            return (T)q.Result;
+        }
+        public void UndoLast()
+        {
+            var e = AllEvents.LastOrDefault();
+            var ac = e as AgeChangedEvent;
+            if (ac != null)
+            {
+                Command(new ChangeAgeCommand(ac.Target, ac.OldValue) { Register = false });
+                AllEvents.Remove(e);
+            }
+        }
+    }
+
+    public class Query
+    {
+        public object Result;
+    }
+    public class AgeQuery : Query
+    {
+        public Person Target;
+
+    }
+    public class Event
+    { 
+        // backtrack
+    }
+    class AgeChangedEvent : Event
+    {
+        public Person Target;
+        public int OldValue, NewValue;
+        public AgeChangedEvent(Person target, int oldValue, int newValue)
+        {
+            this.Target = target;
+            this.OldValue = oldValue;
+            this.NewValue = newValue;
+        }
+        public override string ToString()
+        {
+            return $"Age changed from {OldValue} to {NewValue}.";
+        }
+    }
+    public class Command : EventArgs
+    {
+        public bool Register = true;
+    }
+
+    class ChangeAgeCommand : Command
+    {
+        public Person Target;
+        public int Age;
+        public ChangeAgeCommand(Person target, int age)
+        {
+            Target = target;
+            Age = age;
+
+        }
+    }
     //==============================================================================================
 
 
@@ -30,7 +140,25 @@ namespace Sec26.Bonus161
         }
         static void main()
         {
+            var eb = new EventBroker();
+            var p = new Person(eb);
+            eb.Command(new ChangeAgeCommand(p, 123));
 
+            foreach (var e in eb.AllEvents)
+            {
+                WriteLine(e);
+            }
+
+            int age;
+            age = eb.Query<int>(new AgeQuery { Target = p });
+            WriteLine(age);
+
+            eb.UndoLast();
+            age = eb.Query<int>(new AgeQuery { Target = p });
+            foreach (var e in eb.AllEvents)
+            {
+                WriteLine(e);
+            }
         }
     }
 
@@ -42,7 +170,17 @@ namespace Sec26.Bonus161
         [Test]
         public void BasicTest()
         {
+            var eb = new EventBroker();
+            var p = new Person(eb);
+            eb.Command(new ChangeAgeCommand(p, 123));
 
+            Assert.IsTrue(eb.AllEvents.Count == 1);
+
+            string output = eb.AllEvents[0].ToString();
+            Assert.AreEqual(output, "Age changed from 0 to 123.");
+
+            eb.UndoLast();
+            Assert.IsTrue(eb.AllEvents.Count == 0);
         }
     }
 }
