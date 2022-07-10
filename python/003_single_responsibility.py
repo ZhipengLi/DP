52e6bbd466c1a2281d2e11d81efc8b978af610b9
Charlie Zhipeng Li <zhipli@microsoft.com>
2022-07-10 10:24:36 -0700

    Added python 003

---
+++ python/003_single_responsibility.py
@@ -0,0 +1,42 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+
+    # def load(self, filename):
+    #     pass
+
+    # def load_from_web(self, uri):
+    #     pass
+
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w+')
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry('first entry')
+j.add_entry('second entry')
+print(f'print journal\n{j}')
+
+file = r'c:\temp\journal.txt'
+PersistenceManager.save_to_file(j, file)
