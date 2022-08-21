19dcde6d3ccfcf9300d1a4c38305bd8e8c815ed6
Charlie <ZhipengLi@users.noreply.github.com>
2022-08-21 09:54:30 -0700

    Update 005_Liskov_substitue.py

--- python/005_Liskov_substitue.py
+++ python/005_Liskov_substitue.py
@@ -1,17 +1,25 @@
+# Liskov Substitute principle:
+# Anything that works with parent class, it should still work with child class
+#
 class Rectangle:
     def __init__(self, width, height):
         self._height = height
         self._width = width
     
+    # Mark this function as property, so you call this function like an attribute
+    #
     @property
     def area(self):
         return self._width *self._height
     def __str__(self):
         return f'Width: {self.width}, height: {self._height}'
+    
     @property
     def width(self):
         return self._width
     
+    # this function then can be used in '=' expression
+    #
     @width.setter
     def width(self, value):
         self._width = value
@@ -29,11 +37,16 @@ def use_it(rc):
     rc.height = 10
     expected = int(w*10)
     print(f'Expected an area of {expected}, got {rc.area}')
-    
+
+# This class is considered as a break of Liskov principle, because the use_it scenario
+#   that works with the parent class, no longer works with this child class
+#
 class Square(Rectangle):
     def __init__(self, size):
         Rectangle.__init__(self, size, size)
     
+    # still a property, but we need to specify it is a property override for the parent class
+    #
     @Rectangle.width.setter
     def width(self, value):
         self._width = self._height = value
