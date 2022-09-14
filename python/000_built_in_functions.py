ecb9863d37057e49f6c1bc3c78fa53d026594661
Charlie <ZhipengLi@users.noreply.github.com>
2022-09-13 18:46:43 -0700

    Update 000_built_in_functions.py

--- python/000_built_in_functions.py
+++ python/000_built_in_functions.py
@@ -17,12 +17,30 @@ class Checker:
         print('bytes(测试) result:', bytes('测试', 'utf8'))
         print('callable(super) result:', callable(super))
         print('chr(97) result:', chr(97))
+        print('complex 1+2j:', complex('1+2j'))
+        print('dict():', dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
+        print('dir():', dir())
+        print('divmod(5,2):', divmod(5,2))
+        #seasons = ['spring', 'summer', 'autumn', 'winter']
+        #list_enumerate = dict(enumerate(seasons, start=1))
+        print('enumerate:', dict(enumerate(['spring', 'summer', 'autumn', 'winter'], start=1)))
+        x=1
+        print('eval(x+1):',eval('x+1'))
+        print('filter:', tuple(filter(lambda x:x%2==0, [1,2,3,4,5,6,7])))
+        print('float(-3):', float('-3'))
+        print('format(5) in binary:', format(5,'b'))
+        print('frozenset:', frozenset([1,2,3,4,5,6,7]))
+        
         for num in self.nums:
             print(f'bool() value of {num}:', bool(num))
             if num < 0:
                 print(f'abs() value of {num}:', abs(num))
                 print(f'bin() value of {num}:', bin(num))
-
+    def test_method(self):
+        print('this is the object method')
 list = [1,2,3,0,-1]
 checker = Checker(list)
 checker.check()
+Checker.class_method = classmethod(Checker.test_method)
+Checker.class_method()
+delattr(Checker, 'class_method')
