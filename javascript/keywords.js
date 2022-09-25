ba70e5b4bfb5996209cc68fcbe1a2e7b72b01b59
Charlie <ZhipengLi@users.noreply.github.com>
2022-09-24 19:06:23 -0700

    Create keywords.js

---
+++ javascript/keywords.js
@@ -0,0 +1,19 @@
+function resolveAfter2Seconds(x) {
+  return new Promise((resolve) => {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    setTimeout(() => {
+      fetch(url)
+          .then(response=>resolve(response.json()))
+    }, 2000);
+  });
+}
+
+async function f1() {
+  const x = await resolveAfter2Seconds(10);
+  console.log('json object:', x);
+  return new Promise((resolve)=>{
+  	resolve(x.id);
+  });
+}
+
+f1().then(x=>console.log('prosmise result of x.id:', x));
