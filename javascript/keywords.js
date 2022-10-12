18b25e74ee43af2f2861155f27173bdf60965897
Charlie <ZhipengLi@users.noreply.github.com>
2022-10-11 20:38:45 -0700

    Update keywords.js

--- javascript/keywords.js
+++ javascript/keywords.js
@@ -1,5 +1,8 @@
 //https://www.programiz.com/javascript/keywords-identifiers
 class JsonCaller extends String{ 
+	static staticMethod(){
+    	console.log("called from static method")
+    }
 	constructor(str){
     	super(str)
     }
@@ -13,8 +16,11 @@ class JsonCaller extends String{
     
       return new Promise((resolve) => {
         setTimeout(() => {
-          fetch(this.toString())
+        	try{
+              fetch(this.toString())
               .then(response=>resolve(response.json()))
+            }
+            catch(e){}
         }, 2000);
         console.log("private url:", this.#url)
     });
@@ -34,6 +40,7 @@ async function f1() {
   let caller = new JsonCaller('https://jsonplaceholder.typicode.com/todos/1');
   //const x = await resolveAfter2Seconds(10);
   console.log('caller instance of JsonCaller?', caller instanceof JsonCaller)
+  JsonCaller.staticMethod()
   const x = await caller.getJson();
   caller = null;
   console.log('json object:', x);
@@ -48,33 +55,6 @@ testArray = [true, String('2'), 100, 4, false]
 console.log('2 in testArray?', '2' in testArray)
 delete testArray[3]
 console.log('after delete testArray[3]', testArray[3])
-let i=0;
-do{
-	switch(typeof(testArray[i++])){
-    	case 'boolean':
-        	continue;
-    	case 'string':
-        	console.log('found a string')
-        	break;
-        case 'number':
-        	if(testArray[i-1]<100){
-            	console.log('num < 100')
-            }
-            else {
-            	console.log('num>=100')
-            }
-        	break;
-        default:
-        	console.log('other type')
-    }
-    //i+=1;
-} while(i<testArray.length)
-
-f1().catch(e=>{console.log('error found:', e)}).then(x=>console.log('prosmise result of x.id:', x)).finally(()=>{console.log('finally by promise')});
-
-testArray = [true, String('2'), 100, 4, false]
-console.log('2 in testArray?', '2' in testArray)
-delete testArray[3]
 let i=0;
 do{
 	switch(typeof(testArray[i++])){
