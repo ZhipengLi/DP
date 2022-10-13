58c0db3b9f0aa30040b1e3ed63da3dfd7e29bc15
Charlie <ZhipengLi@users.noreply.github.com>
2022-10-13 16:36:07 -0700

    Create single_responsibility_principle.ts

---
+++ typescript/single_responsibility_principle.ts
@@ -0,0 +1,66 @@
+class Journal
+{
+  static count:number;
+  private entries:string[]
+  constructor() {
+    this.entries = [];
+    Journal.count = 0;
+  }
+
+  addEntry(text:string) : number
+  {
+    let c = ++Journal.count;
+    let entry = `${c}: ${text}`;
+    this.entries[c] = entry;
+    return c;
+  }
+
+  removeEntry(index:number)
+  {
+    delete this.entries[index];
+  }
+
+  toString()
+  {
+    return Object.values(this.entries).join('\n');
+  }
+
+  // save(filename)
+  // {
+  //   fs.writeFileSync(filename, this.toString());
+  // }
+  //
+  // load(filename)
+  // {
+  //   //
+  // }
+  //
+  // loadFromUrl(url)
+  // {
+  //   //
+  // }
+}
+Journal.count = 0;
+
+class PersistenceManager
+{
+  preprocess()
+  {
+    //
+  }
+
+  saveToFile(journal:Journal, filename:string)
+  {
+    //fs.writeFileSync(filename, journal.toString());
+    console.log(filename, journal.toString());
+  }
+}
+
+let j = new Journal();
+j.addEntry('I cried today.');
+j.addEntry('I ate a bug.');
+console.log(j.toString());
+
+let p =new PersistenceManager();
+let filename = 'c:/temp/journal.txt';
+p.saveToFile(j, filename);
