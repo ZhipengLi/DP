d002461b27cd35c8f2953050194bc2051b278e51
Charlie <ZhipengLi@users.noreply.github.com>
2022-10-15 18:12:05 -0700

    Create 012_typescript_classes_member_visibility_and_implements.ts

---
+++ typescript/NOBSTS/012_typescript_classes_member_visibility_and_implements.ts
@@ -0,0 +1,35 @@
+interface Database {
+    get(id: string): string;
+    set(id:string, value:string ): void;
+}
+interface Persistable {
+    saveToString(): string;
+    restoreFromString(storedState: string):void;
+}
+class InMemoryDatabase implements Database {
+    protected db: Record<string, string> = {};
+    get(id: string): string {
+        return this.db[id];
+    }
+    set(id:string, value:string ): void{
+        this.db[id] = value;
+    }
+}
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable{
+    saveToString(): string{
+        return JSON.stringify(this.db);
+    }
+    restoreFromString(storedState: string):void{
+        this.db = JSON.parse(storedState);
+    }
+}
+//const myDB = new InMemoryDatabase();
+const myDB = new PersistentMemoryDB();
+myDB.set("foo", "bar");
+//myDB.db["foot] = "baz";
+console.log(myDB.get("foo"));
+const saved = myDB.saveToString();
+
+const myDB2 = new PersistentMemoryDB();
+myDB2.restoreFromString(saved);
+console.log(myDB2.get("foo"));
