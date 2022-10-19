c5bfc430582d0a4737cb4836eef248a8e54f4926
Charlie <ZhipengLi@users.noreply.github.com>
2022-10-18 19:19:06 -0700

    Create 021_typescript_react_hooks.ts

---
+++ typescript/NOBSTS/021_typescript_react_hooks.ts
@@ -0,0 +1,125 @@
+import React, {useCallback, useEffect, useState, useReducer, useRef} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface BoxProps {
+  children?: React.ReactNode;
+}
+
+const Heading = ({title}: {title: string})=>(
+  <h2>{title}</h2>
+)
+
+//const Box = ({children}: {children: React.ReactNode}) => (
+const Box:React.FunctionComponent<BoxProps> = ({children}) => (
+  <div style = {{
+    padding: "1rem",
+    fontWeight: "bold"
+  }}>
+    {children}
+  </div>
+)
+
+const List:React.FC<{
+  items: string[];
+  onClick?: (item: string) => void
+}> = ({items, onClick}) =>(
+  <ul>
+    {
+      items.map((item, index) => (
+      <li key={index} onClick={()=> onClick?.(item)}>{item}</li>
+      ))
+    }
+  </ul>
+);
+interface Payload {
+  text: string;
+}
+
+interface Todo {
+  id: number;
+  done: boolean;
+  text: string;
+}
+
+type ActionType = 
+  | {type: "ADD", text: string}
+  | {type: "REMOVE", id: number};
+
+function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(()=>{
+    console.log('state begin');
+    fetch("/data.json")
+    .then((resp) => resp.json())
+    .then((data)=>{
+      setPayload(data);
+      console.log('state set');
+    });
+  }, []);
+
+  const [todos, dispatch] = useReducer(
+    (state: Todo[], action: ActionType) => {
+      switch(action.type){
+        case "ADD":
+          return [
+            ...state,
+            {
+              id: state.length,
+              text: action.text,
+              done: false,
+            }
+          ]
+        case "REMOVE":
+          return state.filter(({id})=>id !== action.id);
+        default:
+          throw new Error();
+      }
+  }, []);
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const onAddTodo = useCallback(()=>{
+    if(newTodoRef.current){
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value
+      });
+      newTodoRef.current.value = "";
+    }
+
+  }, []);
+
+  return (
+    <div>
+      <Heading title="Introduction" />
+      <Box>
+        Hello there
+      </Box>
+      <List items = {["one", "two", "three"]} onClick={onListClick}></List>
+      <Box>{JSON.stringify(payload)}</Box>
+      <Heading title="Todos" />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={()=>dispatch({
+            type: "REMOVE",
+            id: todo.id,
+          })}>
+          Remove
+          </button>
+        </div>
+      ))}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add Todo</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
