3503c8d009d9bf25c8d6149ae65447ed6c836c76
Charlie <ZhipengLi@users.noreply.github.com>
2022-11-13 12:34:13 -0800

    Update 06_useReducer_todo.tsx

--- typescript/hooks/06_useReducer_todo.tsx
+++ typescript/hooks/06_useReducer_todo.tsx
@@ -2,28 +2,32 @@ import React, { useReducer, useState, Dispatch } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { CombinedCodeActions, setTokenSourceMapRange } from "typescript";
+import { time } from "console";
+import { create } from "domain";
 
 type TodoType = {
   id: number;
   name: string;
   completed: boolean;
 };
-
-type ActionType =
-  | { type: ACTIONS.ADD_TODO; payload: { name: string } }
-  | { type: ACTIONS.TOGGLE_TODO; payload: { id: number } }
-  | { type: ACTIONS.DELETE_TODO; payload: { id: number } };
-
 enum ACTIONS {
   ADD_TODO,
   TOGGLE_TODO,
-  DELETE_TODO,
+  REMOVE_TODO,
 }
-
-function reducer(todos: TodoType[], action: ActionType): TodoType[] {
+type ActionType =
+  | { type: ACTIONS.ADD_TODO; payload: { name: string } }
+  | { type: ACTIONS.TOGGLE_TODO; payload: { id: number } }
+  | { type: ACTIONS.REMOVE_TODO; payload: { id: number } };
+function Reducer(todos: TodoType[], action: ActionType): TodoType[] {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)];
+      const newTodo: TodoType = {
+        id: Date.now(),
+        name: action.payload.name,
+        completed: false,
+      };
+      return [...todos, newTodo];
     case ACTIONS.TOGGLE_TODO:
       return todos.map((todo) => {
         if (todo.id === action.payload.id) {
@@ -31,15 +35,12 @@ function reducer(todos: TodoType[], action: ActionType): TodoType[] {
         }
         return todo;
       });
-    case ACTIONS.DELETE_TODO:
+    case ACTIONS.REMOVE_TODO:
       return todos.filter((todo) => todo.id != action.payload.id);
     default:
       return todos;
   }
-}
-
-function newTodo(name: string): TodoType {
-  return { id: Date.now(), name: name, completed: false };
+  return [];
 }
 
 function Todo({
@@ -50,51 +51,47 @@ function Todo({
   dispatch: Dispatch<ActionType>;
 }) {
   return (
-    <div>
-      <span style={{ color: todo.completed ? "#777" : "#000" }}>
-        {todo.name}
-      </span>
+    <>
+      <div style={{ color: todo.completed ? "#ccc" : "#000" }}>{todo.name}</div>
       <button
-        onClick={() =>
-          dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })
-        }
+        onClick={() => {
+          dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } });
+        }}
       >
         Toggle
       </button>
       <button
-        onClick={() =>
-          dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })
-        }
+        onClick={() => {
+          dispatch({ type: ACTIONS.REMOVE_TODO, payload: { id: todo.id } });
+        }}
       >
-        Delete
+        X
       </button>
-    </div>
+    </>
   );
 }
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, [] as TodoType[]);
+  const [todos, dispatch] = useReducer(Reducer, [] as TodoType[]);
   const [name, setName] = useState("");
-
-  function handleSubmit(e: React.FormEvent) {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
     setName("");
-  }
-
-  console.log(todos);
-
+  };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
       </form>
       {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch}></Todo>;
       })}
     </>
   );
